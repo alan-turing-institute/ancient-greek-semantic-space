aaeb61e0f8353cacc9802411952c68a071bfefe7
Barbara McGillivray <bmcgillivray@turing.ac.uk>
2019-03-27 21:23:09 +0000

    Update evaluation_text_processing.py

--- evaluation_text_processing.py
+++ evaluation_text_processing.py
@@ -844,7 +844,7 @@ for [lemma1, lemma2] in agwn_cooccurrence:
                                                              dissect_lemma2coordinates[lemma2])
 
         if count_n % 1000 == 0:
-            print("Consider synonyms", lemma1, "and", lemma2, "AGWN IDs are", str(id1), "and", str(id2), "AGWN cosine distance:", str(agwn_cos_distance_lemma1_lemma2), "DISSECT cosine distance:", str(dissect_cos_distance_lemma1_lemma2))
+            print(str(count_n), "out of", str(len(agwn_cooccurrence)), "Consider synonyms", lemma1, "and", lemma2, "AGWN IDs are", str(id1), "and", str(id2), "AGWN cosine distance:", str(agwn_cos_distance_lemma1_lemma2), "DISSECT cosine distance:", str(dissect_cos_distance_lemma1_lemma2))
 
         dissect_distances_shared_agwn_synonyms.append(dissect_cos_distance_lemma1_lemma2)
 
